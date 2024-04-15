83f35458863a98a983be731281e18443d6534eb3
kjkandrea <kjkandrea@gmail.com>
2024-04-15 15:37:18 +0900

    test: add test queryClient detail option

--- cypress/support/mountWithAppContext.tsx
+++ cypress/support/mountWithAppContext.tsx
@@ -3,7 +3,18 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 export function mountWithAppContext(children: React.ReactNode) {
   return mount(
-    <QueryClientProvider client={new QueryClient()}>
+    <QueryClientProvider
+      client={
+        new QueryClient({
+          defaultOptions: {
+            queries: {
+              retry: 0,
+              refetchOnWindowFocus: false,
+            },
+          },
+        })
+      }
+    >
       {children}
     </QueryClientProvider>,
   );
