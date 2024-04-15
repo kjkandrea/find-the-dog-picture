0173656029213add5461be0ee65443eaaba09479
kjkandrea <kjkandrea@gmail.com>
2024-04-15 10:00:28 +0900

    feat: seed

--- src/api/pictureQuiz.ts
+++ src/api/pictureQuiz.ts
@@ -1,6 +1,7 @@
 import { cats } from "./images/cats";
 import { dogs } from "./images/dogs";
 import { sampleSize } from "lodash-es";
+import { pickRandomIndex } from "~/utils";
 
 type ResourceURL = string;
 type UniqueId = number;
@@ -15,17 +16,35 @@ export interface GetPictureQuizResponse {
 }
 
 class PictureQuiz {
+  private previousDogIndexesSetBySeed: Map<number, Set<number>> = new Map();
   public getPictureQuiz(
-    // more than 1
+    // 2 이상
     pictureLength: number = 4,
     seed: number = 0,
   ): Promise<GetPictureQuizResponse> {
     if (1 >= pictureLength) {
       throw new Error("bad request: pictureLength must be more than 1");
     }
+
+    // 비즈니스 로직. 세션 당 10번 까지 지난 결과를 기록해두고, 10번을 초과하면 초기화
+    const hasMax =
+      this.previousDogIndexesSetBySeed.has(seed) &&
+      this.previousDogIndexesSetBySeed.get(seed)!.size === 10;
+
+    if (!this.previousDogIndexesSetBySeed.has(seed) || hasMax) {
+      this.previousDogIndexesSetBySeed.set(seed, new Set());
+    }
+
+    const previousDogIndexesSet = this.previousDogIndexesSetBySeed.get(seed)!;
+    const dogIndex = pickRandomIndex(dogs.length, previousDogIndexesSet);
+
+    previousDogIndexesSet.add(dogIndex);
+
+    this.previousDogIndexesSetBySeed.set(seed, previousDogIndexesSet);
+
     return this.fakeResponseDelay({
       quiz: {
-        pictures: [...sampleSize(cats, pictureLength - 1), dogs[0]],
+        pictures: [...sampleSize(cats, pictureLength - 1), dogs[dogIndex]],
         id: this.generateUniqueId(),
       },
     });
