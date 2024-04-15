0b5f9fdb6b1a46e6f4f21587bb6a597b24e8d821
kjkandrea <kjkandrea@gmail.com>
2024-04-15 09:11:46 +0900

    refactor: test flow

--- src/utils/pickRandomItem.spec.ts
+++ src/utils/pickRandomItem.spec.ts
@@ -8,10 +8,9 @@ describe("pickRandomItem", () => {
 
     for (let i = 0; i < 100; i++) {
       const result = pickRandomItem(array, excludeIndexesSet);
-      expect(result !== undefined && array.includes(result)).toBe(true);
-      expect(
-        result === undefined || excludeIndexes.includes(array.indexOf(result)),
-      ).toBe(false);
+      expect(result).not.toBeUndefined();
+      expect(array.includes(result!)).toBe(true);
+      expect(excludeIndexes.includes(array.indexOf(result!))).toBe(false);
     }
   });
 });
