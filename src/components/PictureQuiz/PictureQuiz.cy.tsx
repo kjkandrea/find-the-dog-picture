cc82b4d0332213ca2469826bd5b3445756495dd1
kjkandrea <kjkandrea@gmail.com>
2024-04-16 17:04:41 +0900

    test: problem level rise

--- src/components/PictureQuiz/PictureQuiz.cy.tsx
+++ src/components/PictureQuiz/PictureQuiz.cy.tsx
@@ -24,13 +24,33 @@ describe("PictureQuiz", () => {
     cy.contains("오답입니다.");
   });
 
-  it("정답 혹은 오답 시 약 2초 후 다음 퀴즈로 넘어감", () => {
+  it("정답 혹은 오답 시 약 2초 후 다음 퀴즈로 넘어간다.", () => {
     cy.clock();
     cy.mount(<PictureQuiz />);
     cy.contains("퀴즈 1");
     pictureGrid().get("img").first().parent().click();
     cy.tick(2000);
     cy.contains("퀴즈 2");
+  });
+
+  it.only(`퀴즈는 2 * 2 그리드로 시작한다.
+    정답을 2번 맞출 시마다 그리드의 크기가 1씩 증가한다. 
+    그리드의 크기는 5 * 5 까지이다. 
+  `, () => {
+    cy.clock();
+    cy.mount(<PictureQuiz />);
+
+    Cypress._.times(10, (i) => {
+      cy.log(`${i + 1} 번째 문제`);
+
+      pictureGrid()
+        .get("img")
+        .should("have.length", Math.floor(i / 2 + 2) ** 2);
+
+      pictureGrid().get("img[src*='dog']").parent().click();
+      cy.tick(2000);
+      cy.wait(100);
+    });
   });
 
   function pictureGrid() {
