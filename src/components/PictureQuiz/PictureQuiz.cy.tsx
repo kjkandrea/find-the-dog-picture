ca0aa8730225df093a13561978a26bfb0940d7b7
kjkandrea <kjkandrea@gmail.com>
2024-04-16 15:09:48 +0900

    test: PictureQuiz wrong answer

--- src/components/PictureQuiz/PictureQuiz.cy.tsx
+++ src/components/PictureQuiz/PictureQuiz.cy.tsx
@@ -1,11 +1,21 @@
 import { PictureQuiz } from ".";
 
 describe("PictureQuiz", () => {
-  // TODO: 다음 문항 출력 테스트
-  it("강아지 사진을 동작 시 정답 처리", () => {
+  it("강아지 사진을 동작 시 정답", () => {
     cy.mount(<PictureQuiz />);
 
     cy.get("img[src*='dog']").parent().click().should("have.class", "correct");
     cy.contains("정답입니다!");
+  });
+
+  it("고양이 사진을 동작 시 오답", () => {
+    cy.mount(<PictureQuiz />);
+
+    cy.get("img[src*='cat']")
+      .first()
+      .parent()
+      .click()
+      .should("have.class", "wrong");
+    cy.contains("오답입니다.");
   });
 });
