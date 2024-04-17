70d3c3be0dc58401cba778969a4ce9f04183983a
kjkandrea <kjkandrea@gmail.com>
2024-04-17 12:39:43 +0900

    test: solve error case

--- src/hooks/useQuiz/useQuiz.spec.ts
+++ src/hooks/useQuiz/useQuiz.spec.ts
@@ -1,14 +1,14 @@
 import { useQuiz } from ".";
 import { Feedback } from "./partials";
 import { describe, it, expect, jest } from "@jest/globals";
+import { usePictureQuizQuery } from "./partials";
 import { renderHook, act, waitFor } from "@testing-library/react";
 
 jest.useFakeTimers();
 
 jest.mock("./partials", () => ({
   usePictureQuizQuery: jest.fn().mockReturnValue({
-    data: { id: 1, pictures: ["dog", "cat"] },
-    refetch: () => new Promise((resolve) => resolve(true)),
+    data: undefined,
   }),
   useSubmitPictureQuizMutation: jest.fn().mockImplementation((unknown) => {
     const options = unknown as { onSuccess: (feedback: Feedback) => void };
@@ -24,7 +24,18 @@ jest.mock("./partials", () => ({
 }));
 
 describe("useQuiz", () => {
+  it("퀴즈가 반환되지 않았을 때에, solve 로 문제를 풀면 에러가 발생한다.", async () => {
+    const { result } = renderHook(() => useQuiz());
+
+    expect(() => result.current.solve(0)).toThrow();
+  });
+
   it("step 은 1로 시작하며, solve 로 문제를 풀 때마다 정/오답에 관계없이 1씩 증가한다.", async () => {
+    (usePictureQuizQuery as jest.Mock).mockReturnValue({
+      data: { id: 1, pictures: ["dog", "cat"] },
+      refetch: () => new Promise((resolve) => resolve(true)),
+    });
+
     const { result } = renderHook(() => useQuiz());
 
     expect(result.current.step).toBe(1);
