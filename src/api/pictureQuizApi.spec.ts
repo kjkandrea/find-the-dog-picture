a04c8846c95a111986c952a2493bf0082eeb8345
kjkandrea <kjkandrea@gmail.com>
2024-04-17 13:35:35 +0900

    refactor: test desc

--- src/api/pictureQuizApi.spec.ts
+++ src/api/pictureQuizApi.spec.ts
@@ -6,12 +6,12 @@ describe("pictureQuizApi", () => {
     pictureQuiz.reset();
   });
 
-  it("getPictureQuiz should return a Quiz", async () => {
+  it("getPictureQuiz 는 퀴즈를 반환한다.", async () => {
     const result = await pictureQuizApi.getPictureQuiz(2);
     expect(result.quiz).toBeTruthy();
   });
 
-  it("postPictureQuiz should return a Result", async () => {
+  it("postPictureQuiz 는 정답여부를 반환한다.", async () => {
     const { quiz } = await pictureQuizApi.getPictureQuiz(2);
     const result = await pictureQuizApi.postPictureQuiz(quiz.id, 0);
     expect(typeof result.correct).toBe("boolean");
