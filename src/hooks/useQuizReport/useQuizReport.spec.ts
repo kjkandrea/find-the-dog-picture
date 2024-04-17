f61f9880f615c32e5831a2ff8532e0c1add1abd6
kjkandrea <kjkandrea@gmail.com>
2024-04-17 13:37:10 +0900

    test: useQuizReport CRUD

---
+++ src/hooks/useQuizReport/useQuizReport.spec.ts
@@ -0,0 +1,38 @@
+import { useQuizReport, QuizSolveLog } from ".";
+import { describe, it, expect, jest } from "@jest/globals";
+import { renderHook, act } from "@testing-library/react";
+import { useState } from "react";
+
+jest.mock("recoil", () => ({
+  atom: jest.fn(),
+  useRecoilState: jest.fn((initValue) => useState(initValue)),
+}));
+
+describe("useQuizReport", () => {
+  it("레포트를 작성하고 제출하면 레포트가 반환된다. 레포트를 초기화하면 레포트가 지워진다.", () => {
+    const { result } = renderHook(() => useQuizReport());
+
+    const quizSolveLog: QuizSolveLog = {
+      quizId: 1,
+      squareMatrix: 2,
+      correct: true,
+      answer: 1,
+    };
+
+    act(() => result.current.add(quizSolveLog));
+
+    expect(result.current.quizReport).toBeUndefined();
+
+    act(() => result.current.submit());
+
+    expect(result.current.quizReport?.quizSolveLogs).toEqual([quizSolveLog]);
+
+    act(() => result.current.reset());
+
+    expect(result.current.quizReport).toBeUndefined();
+
+    act(() => result.current.submit());
+
+    expect(result.current.quizReport?.quizSolveLogs).toEqual([]);
+  });
+});
